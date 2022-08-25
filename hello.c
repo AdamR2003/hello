7227bcd035d08c3aca3efe091a94c7cbef7bea0e
Adam Romero <aromero9@huskers.unl.edu>
2022-08-25 18:50:33 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Adam Romero
+ *         aromero9@huskers.unl.edu
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("Adam Romero\n");
+
+  return 0;
+}
